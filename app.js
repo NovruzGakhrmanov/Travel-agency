b0fadd7c73ea79646e551dc68c69564ffd167ca4
Novruz Gakhramanov <novruzgaxramanov@gmail.com>
2022-10-23 17:02:58 +0400

    Second Commit

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
 }
 
-// video 
-document.querySelectorAll( '.about .video-container .controls .control-btn').forEach(btn =>{
-    btn.onclick = () => {
-        let src =btn.getAttribute('data-src');
-        document.querySelector('.about .video-container .video').src= src;
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+}
+
+document.querySelectorAll('.about .video-container .controls .control-btn').forEach(btn =>{
+    btn.onclick = () =>{
+        let src = btn.getAttribute('data-src');
+        document.querySelector('.about .video-container .video').src = src;
     }
 })
